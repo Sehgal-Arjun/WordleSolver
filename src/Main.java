47dba8edc123bed23ee078c58eb6b1d7f30d8438
Arjun-Sehgal <arjun_sehgal@asl.org>
2022-02-17 12:57:59 +0000

    Working version
    Still only picking a random option

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 
+    //public static String[] validGuesses = new String[10657];
+    public static String[] validGuesses = new String[12972];
+    public static String[] validAnswers = new String[2315];
+    public static ArrayList<Character> blackLetters = new ArrayList<Character>();
+    public static ArrayList<String> refinedAnswers = new ArrayList<String>();
+    public static int count = 0;
+
     public static void main(String[] args) {
 	// write your code here
+        setVariables();
+        boolean finished = false;
+        while (!finished){
+            finished = findWord();
+        }
+    }
+
+    public static void setVariables(){
         int tracker = 0;
         int tracker2 = 0;
-        String[] validGuesses = new String[10657];
-        String[] validAnswers = new String[2315];
+        
         Scanner sc = null;
         try {
             sc = new Scanner(new File("/Users/arjun/Documents/GitHub/WordleSolver/valid_guesses.csv"));
@@ -40,5 +56,94 @@ public class Main {
             }
         }
         sc2.close();  //closes the scanner
+
+        for (int i = 0; i < validAnswers.length; i++){
+            validGuesses[10657 + i] = validAnswers[i];
+        }
+
+        for (int i = 0; i < validGuesses.length; i++){
+            refinedAnswers.add(validGuesses[i]);
+        }
+    }
+
+    public static boolean findWord(){
+        Character[] result = {'b', 'b', 'b', 'b', 'b'};
+        Character[] test = {'g', 'g', 'g', 'g', 'g'};
+        ArrayList<String> removeList = new ArrayList<String>();
+        Character[] word = {'s', 'a', 'l', 'e', 't'};
+        Scanner scn = new Scanner(System.in);  // Create a Scanner object for getting the word the user inputted
+        System.out.println("Your inputted word: ");
+        String response = scn.nextLine(); 
+        if (!response.equals("reroll")){
+            for (int i = 0; i < word.length; i++){
+                word[i] = response.substring(i, i+1).charAt(0);
+            }
+    
+            for (int i = 0; i < word.length; i++){
+                System.out.println("Wordle output for: " + word[i] + "?");
+                String out = scn.nextLine();  // Read user input
+                result[i] = out.charAt(0); // Assign the output to a results array
+            }
+            //scn.close();
+            for (int i = 0; i< result.length; i++){
+                if (result[i] == 'b'){
+                    for (int j = 0; j < refinedAnswers.size(); j++){
+                        if (refinedAnswers.get(j).indexOf(word[i]) == -1){
+                            if (!blackLetters.contains(word[i])){
+                                blackLetters.add(word[i]);
+                            }
+                        }
+                    }
+                }
+                if (result[i] == 'y'){
+                    for (int j = 0; j < refinedAnswers.size(); j++){
+                        if (refinedAnswers.get(j).indexOf(word[i]) != -1){
+                            if (refinedAnswers.get(i).indexOf(word[i]) == i){
+                                if(!refinedAnswers.contains(refinedAnswers.get(j))){ 
+                                    removeList.add(refinedAnswers.get(j));
+                                }
+                            }
+                        }
+                        else{
+                            removeList.add(refinedAnswers.get(j));
+                        }
+                    }
+                }
+                if (result[i] == 'g'){
+                    for (int j = 0; j < refinedAnswers.size(); j++){
+                        if (refinedAnswers.get(j).indexOf(word[i]) != i){
+                            removeList.add(refinedAnswers.get(j));
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < refinedAnswers.size(); i++){
+                boolean contains = false;
+                for (int j = 0; j < blackLetters.size(); j++){ //breakpoint
+                    if (refinedAnswers.get(i).contains(blackLetters.get(j) + "")){
+                        contains = true;
+                    }
+                } //breakpoint
+                
+                if (contains){
+                    removeList.add(refinedAnswers.get(i));
+                }
+            }
+            for (int i = 0; i < removeList.size(); i++){
+                refinedAnswers.remove(removeList.get(i));
+            }
+            count++;
+        }
+        
+
+        if (result == test){
+            System.out.println("Nice! Got it in " + count + " tries!");
+            return true;
+        }
+
+        Random rand = new Random();
+        System.out.println("Recommendation: " + refinedAnswers.get(rand.nextInt(refinedAnswers.size())));
+
+        return false;
     }
 }
